9767c802a88bda3348731731eb548326b979c22d
Jagan Karunakaran <jagan.karan13@gmail.com>
2021-02-19 17:25:30 +0530

    Initial Commit

---
+++ src/main/java/com/example/springintegrationlearning/SpringIntegrationLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.springintegrationlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationLearningApplication.class, args);
+	}
+
+}
